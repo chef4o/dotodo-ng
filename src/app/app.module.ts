24066c4cd27a1a327ec8ef96ba65d7c4b9abaf1a
Stefan Hristov <sihristov@hotmail.com>
2024-02-08 21:41:52 +0200

    Move common components ot app/cor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { TopbarNavComponent } from './nav/topbar-nav/topbar-nav.component';
-import { SidebarNavComponent } from './nav/sidebar-nav/sidebar-nav.component';
-import { LoginModalComponent } from './auth/login-modal/login-modal.component';
-import { RegisterModalComponent } from './auth/register-modal/register-modal.component';
-import { ConnectNavComponent } from './nav/connect-nav/connect-nav.component';
-import { AuthComponent } from './auth/auth.component';
-import { BottombarNavComponent } from './nav/bottombar-nav/bottombar-nav.component';
-import { FooterComponent } from './footer/footer.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    TopbarNavComponent,
-    SidebarNavComponent,
-    ConnectNavComponent,
-    BottombarNavComponent,
-    AuthComponent,
-    LoginModalComponent,
-    RegisterModalComponent,
-    FooterComponent
-  ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
+import { BottombarNavComponent } from './nav/bottombar-nav/bottombar-nav.component';
+import { ConnectNavComponent } from './nav/connect-nav/connect-nav.component';
+import { SidebarNavComponent } from './nav/sidebar-nav/sidebar-nav.component';
+import { TopbarNavComponent } from './nav/topbar-nav/topbar-nav.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginModalComponent } from './auth/login-modal/login-modal.component';
+import { RegisterModalComponent } from './auth/register-modal/register-modal.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    NavComponent,
+    BottombarNavComponent,
+    ConnectNavComponent,
+    SidebarNavComponent,
+    TopbarNavComponent,
+    AuthComponent,
+    LoginModalComponent,
+    RegisterModalComponent,
+    FooterComponent,
+  ],
+  imports: [CommonModule],
+  exports: [NavComponent, FooterComponent],
+})
+export class CoreModule {}

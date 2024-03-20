bc21fa21301e7832d6c9baaec2c1cda59f9063b0
Stefan Hristov <sihristov@hotmail.com>
2024-03-20 19:52:59 +0200

    Remove unnecessary code

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,32 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NewsComponent } from './news/news.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { SharedModule } from '../shared/shared.module';
-import { HomeModule } from './home/home.module';
-import { ProfileModule } from './profile/profile.module';
-import { NotesModule } from './notes/notes.module';
-import { EventsModule } from './events/events.module';
 import { MainComponent } from './main.component';
 import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  declarations: [
-    MainComponent,
-    NewsComponent,
-    AboutUsComponent,
-    ContactsComponent,
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    HomeModule,
-    EventsModule,
-    NotesModule,
-    ProfileModule,
-    AppRoutingModule,
-  ],
-  exports: [MainComponent, NewsComponent, AboutUsComponent, ContactsComponent],
+  declarations: [MainComponent],
+  imports: [AppRoutingModule],
+  exports: [MainComponent],
 })
 export class MainModule {}

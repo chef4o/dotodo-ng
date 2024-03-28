0a29f00566c6a06f92c173723d223f04b819c445
Stefan Hristov <stefan.hristov@paysafe.com>
2024-03-29 00:47:35 +0200

    refactor auth routings

--- src/app/core/auth/auth-routing.module.ts
+++ src/app/core/auth/auth-routing.module.ts
@@ -4,17 +4,22 @@ import { LogoutComponent } from "./logout/logout.component";
 
 const routes: Routes = [
     {
-        path: 'auth/login',
-        component: AuthComponent
-    },
-    {
-        path: 'auth/register',
-        component: AuthComponent
-    },
-    {
-        path: 'auth/logout',
-        component: LogoutComponent
-    },
+        path: 'auth',
+        children: [
+            {
+                path: 'login',
+                component: AuthComponent
+            },
+            {
+                path: 'register',
+                component: AuthComponent
+            },
+            {
+                path: 'logout',
+                component: LogoutComponent
+            }
+        ]
+    }
 ];
 
 export const AuthRoutingModule = RouterModule.forChild(routes);

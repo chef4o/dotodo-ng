93f91528c4f2cbf3ebeff42e4ad3c21c77cd2543
Stefan Hristov <stefan.hristov@paysafe.com>
2024-03-28 21:30:08 +0200

    moving all routing in app-routing module and adding error page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { NavModule } from './nav/nav.module';
 import { AuthModule } from './auth/auth.module';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
-  declarations: [FooterComponent],
+  declarations: [FooterComponent, ErrorComponent],
   imports: [CommonModule, NavModule, AuthModule],
   exports: [NavModule, FooterComponent],
 })

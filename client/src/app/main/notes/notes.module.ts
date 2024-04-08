9374a08182a321837ed2b95f1a1525dd00d1344a
Stefan Hristov <sihristov@hotmail.com>
2024-04-09 00:10:14 +0300

    add notes module

--- client/src/app/main/notes/notes.module.ts
+++ client/src/app/main/notes/notes.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NotesComponent } from './notes.component';
 import { EmptyNoteComponent } from './empty-note/empty-note.component';
 import { NoteItemComponent } from './note-item/note-item.component';
+import { ErrorHandlingModule } from 'src/app/shared/error-handling/error-handling.module';
+import { ErrorPagesModule } from 'src/app/shared/error-handling/error-pages/error-pages.module';
 
 @NgModule({
   declarations: [NotesComponent, EmptyNoteComponent, NoteItemComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ErrorPagesModule],
   exports: [NotesComponent],
 })
 export class NotesModule {}

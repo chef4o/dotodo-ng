225f2a6700e97e99148d3f9ca07a6d9f404b61eb
Stefan Hristov <sihristov@hotmail.com>
2024-04-11 21:09:47 +0300

    add lazy loading

--- client/src/app/main/main.module.ts
+++ client/src/app/main/main.module.ts
@@ -5,36 +5,29 @@ import { MainComponent } from './main.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { CoreModule } from '../core/core.module';
 import { HomeModule } from './home/home.module';
-import { EventsModule } from './events/events.module';
-import { NotesModule } from './notes/notes.module';
-import { ProfileModule } from './profile/profile.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContactsComponent } from './contacts/contacts.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { NewsComponent } from './news/news.component';
-import { ChecklistsModule } from './checklists/checklists.module';
-import { ShrinkToFitPipe } from '../shared/pipes/shrink-to-fit.pipe';
+import { BingMapComponent } from './about-us/bing-map/bing-map.component';
 
 @NgModule({
   declarations: [
-    MainComponent, 
+    MainComponent,
     ContactsComponent,
-    AboutUsComponent, 
-    NewsComponent
+    AboutUsComponent,
+    NewsComponent,
+    BingMapComponent,
   ],
   imports: [
     FormsModule,
     ReactiveFormsModule,
     HomeModule,
-    EventsModule,
-    NotesModule,
-    ChecklistsModule,
-    ProfileModule,
     CoreModule,
     CommonModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   exports: [MainComponent],
 })
-export class MainModule { }
+export class MainModule {}
